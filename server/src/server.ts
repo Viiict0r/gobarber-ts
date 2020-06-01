cde3fd31149a25dfbd47574178a036c98e9ef415
Victor Hugo <viiict0r@hotmail.com>
2020-05-31 23:15:29 -0300

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+/** Keep It Simple & Stupid */
+
+app.listen(3333, () => {
+  console.log('Server running in port 3333!');
+});
---
+++ server/src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from 'typeorm';
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  date: Date;
+  provider_id: string;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = await appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
