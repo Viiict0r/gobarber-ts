dd395270270f3b0c839557a4342141f4873d420b
Victor Hugo <viiict0r@hotmail.com>
2020-06-04 22:07:49 -0300

    chore: Error handling, and avatar update

--- server/src/routes/sessions.routes.ts
+++ server/src/routes/sessions.routes.ts
@@ -5,17 +5,13 @@ import AuthenticatedUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticatedUserService();
+  const authenticateUser = new AuthenticatedUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -3,6 +3,8 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -22,13 +24,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { expiresIn, secret } = authConfig.jwt;
@@ -37,6 +39,8 @@ class AuthenticateUserService {
       subject: user.id,
       expiresIn,
     });
+
+    delete user.password;
 
     return {
       user,
